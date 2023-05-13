15731988d2ba60f58f0b76bd4c2c3189efa90caf
jhelioneto <jhelionetodev@gmail.com>
2023-05-13 15:43:51 -0300

    apagado o restricti mode

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -2,7 +2,13 @@ import estilo from './styles.module.scss'
 import Filtrar from '../../components/MenuFiltrar'
 import Produtos from '../../components/ProdutoCard'
 import Ordenar from '../../components/MenuOrdenar'
+import { useEffect } from 'react'
 export default function ProdutoHome() {
+
+    useEffect(()=>{
+        console.log(1)
+        fetch('https://projeto-backend-te6l.onrender.com/produtos').then(res=>res.json()).then(data => console.log(data))
+    }, [])
     return (
         <div className={estilo.teste}>
             <div className={estilo.container}>
@@ -14,8 +20,11 @@ export default function ProdutoHome() {
                         <Filtrar />
                     </div>
                     <div className={estilo.produtos}>
+                        1
                         <Produtos />
+                        2
                         <Produtos />
+                        3
                         <Produtos />
                         <Produtos />
                         <Produtos />
